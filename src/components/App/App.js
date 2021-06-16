444f538d114214fa55fb0d7b1e80b214f1d4002f
Artem <artem.xolod.90@gmail.com>
2021-06-16 20:54:49 +0300

    setState() - редактирование элементов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,13 +8,22 @@ import ItemAddForm from '../item-add-form/item-add-form';
 
 import './App.css';
 export default class App extends React.Component {
-    maxId = 100;
+    maxId = 1;
     state = {
         todoData: [
-            { label: 'Drink Coffee', important: false, id: 1 },
-            { label: 'Make Awesome App', important: false, id: 2 },
-            { label: 'Have a lunch', important: false, id: 3 }
+            this.createTodoItem('Drink Coffee'),
+            this.createTodoItem('Make Awesome App'),
+            this.createTodoItem('Have a lunch'),
         ]
+    }
+
+    createTodoItem(label) {
+        return {
+            label: label,
+            important: false,
+            done: false,
+            id: this.maxId++
+        }
     }
 
     deleteItem = (id) => {
@@ -30,10 +39,7 @@ export default class App extends React.Component {
     }
 
     addItem = (text) => {
-
-        const newItem = {
-            label: text, important: false, id: this.maxId++
-        }
+        const newItem = this.createTodoItem(text);
 
         this.setState(({ todoData }) => {
             const newArray = [...todoData, newItem];
@@ -42,19 +48,52 @@ export default class App extends React.Component {
                 todoData: newArray
             }
         })
+    }
 
+    toggleProperty(arr, id, propName) {
+        const idx = arr.findIndex((el) => el.id === id);
+        const oldItem = arr[idx];
+        const newItem = { ...oldItem, [propName]: !oldItem[propName] };
+
+        return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
+    }
+
+    onToggleImportant = (id) => {
+        this.setState(({ todoData }) => {
+            return {
+                todoData: this.toggleProperty(todoData, id, 'important')
+            };
+        })
+    }
+
+    onToggleDone = (id) => {
+        this.setState(({ todoData }) => {
+            return {
+                todoData: this.toggleProperty(todoData, id, 'done')
+            };
+        })
     }
 
     render() {
+        const { todoData } = this.state;
+
+        const doneCount = todoData
+            .filter((el) => el.done).length;
+        const todoCount = todoData.length - doneCount;
+
+
         return (
             <div className="todo-app" >
-                <AppHeader toDo={1} done={3} />
+                <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
                     <SearchPanel />
                     <ItemStatusFilter />
                 </div>
 
-                <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+                <TodoList todos={this.state.todoData}
+                    onDeleted={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleDone={this.onToggleDone} />
                 <ItemAddForm onAdd={this.addItem} />
             </div>
         );
--- src/components/todo-list/todo-list-item/todo-list-item.js
+++ src/components/todo-list/todo-list-item/todo-list-item.js
@@ -2,15 +2,8 @@ import React from 'react';
 
 import './todo-list-item.css';
 
-export default class TodoListItem extends React.Component {
+const TodoListItem = ({ label, onDeleted, onToggleDone, onToggleImportant, important, done }) => {
 
-  constructor() {
-    super();
-    this.state = {
-      done: false,
-      important: false
-    };
-  }
 
   // 1-й способ не потерять this при событии
   // В событие передавать onClick={this.onLabelClick.bind(this)} 
@@ -22,53 +15,44 @@ export default class TodoListItem extends React.Component {
   //   }
   // }
   // 3-й способ не потерять this при событии
-  onLabelClick = () => {
-    this.setState(({ done }) => {
-      return { done: !done }
-    });
+  // onLabelClick = () => {
+  //   this.setState(({ done }) => {
+  //     return { done: !done }
+  //   });
+  // }
+
+  let classNames = ['todo-list-item'];
+
+  if (important) {
+    classNames.push('important');
+  }
+  if (done) {
+    classNames.push('done');
   }
 
-  onImportantClick = () => {
-    this.setState(({ important }) => {
-      return { important: !important }
-    });
-  }
-
-  render() {
-    const { label, onDeleted } = this.props;
-    const { done, important } = this.state;
-
-    let classNames = ['todo-list-item'];
-
-    if (important) {
-      classNames.push('important');
-    }
-    if (done) {
-      classNames.push('done');
-    }
-
-    return (
-      <span className={classNames.join(' ')}>
-        <span
-          className="todo-list-item-label"
-          onClick={this.onLabelClick}>
-          {label}
-        </span>
-
-        <button type="button"
-          className="btn btn-outline-success btn-sm float-right"
-          onClick={this.onImportantClick}>
-          <i className="fa fa-exclamation" />
-        </button>
-
-        <button type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-          onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
-        </button>
+  return (
+    <span className={classNames.join(' ')}>
+      <span
+        className="todo-list-item-label"
+        onClick={onToggleDone}>
+        {label}
       </span>
-    );
 
-  }
+      <button type="button"
+        className="btn btn-outline-success btn-sm float-right"
+        onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+        onClick={onDeleted}>
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+
+
 }
 
+export default TodoListItem;
